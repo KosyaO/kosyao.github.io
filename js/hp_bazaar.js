2a07431016c9d6d320cefb70fbcec51fd76c7950
Александр Озеров <cdg.axel@gmail.com>
2023-12-31 15:03:58 +0300

    updated a bit

--- js/hp_bazaar.js
+++ js/hp_bazaar.js
@@ -1,3 +1,8 @@
 function init() {
     document.getElementById('cWelcome').innerHTML = "This is text!"
 }
+
+function navClick(item) {
+    const ctrl = document.getElementById('cWelcome');
+    ctrl.innerHTML = item.text + ' clicked'
+}
