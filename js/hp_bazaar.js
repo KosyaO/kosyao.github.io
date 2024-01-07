2fd69c971d1b0fd544a9f5627446201c8d10d1f6
Александр Озеров <cdg.axel@gmail.com>
2024-01-07 21:28:42 +0300

    separated drawing from updating

--- js/hp_bazaar.js
+++ js/hp_bazaar.js
@@ -30,12 +30,27 @@ function formatNumber(num, maximumFractionDigits=2) {
     return new Intl.NumberFormat(lang, {maximumFractionDigits, minimumFractionDigits: maximumFractionDigits}).format(num) + postfix;
 }
 
-function drawMarket() {
-
+function updateCrop(product, marketCrop) {
+    if (product[0] == "-") return;
+    let sell_price, sell_changes, buy_price, buy_changes, spread, buy_move, move_changes;
+    sell_price = averagePrice(marketCrop.sell_summary);
+    const s_old = oldMarket[product]?.sell_price;
+    sell_changes = s_old? (sell_price - s_old) / s_old * 100: 0;
+    buy_price = averagePrice(marketCrop.buy_summary);
+    const b_old = oldMarket[product]?.buy_price;
+    buy_changes = b_old? (buy_price - b_old) / b_old * 100: 0;
+    spread = buy_price? (buy_price - sell_price) / sell_price * 100 : 999;
+    if (spread > 999.99) spread = 999.99;
+    buy_move = marketCrop.quick_status.buyMovingWeek;
+    move_changes = buy_move - (oldMarket[product]?.buy_move ?? buy_move);
+    const {low, high} = config.alerts_dict[product] ?? {};
+    const color_map = {r: ' class="table-danger"', g: ' class="table-success"', d: ''};
+    const color_buy = color_map[buy_price <= low ? 'r' : (buy_price >= high ? 'g' : 'd')];
+    const color_sell = color_map[sell_price >= high ? 'r' : (sell_price <= low ? 'g' : 'd')];
+    oldMarket[product] = {sell_price, sell_changes, buy_price, buy_changes, spread, buy_move, move_changes, color_buy, color_sell};
 }
 
-function updateMarket(market) {
-    if (!market.success) return updateStatus('Error loading market data');
+function drawMarket() {
     let tableData = "";
     let menuItems;
     if (selectedMenu == alertMenu) menuItems = config.Alerts.map(item => item.name);
@@ -44,38 +59,35 @@ function updateMarket(market) {
         if (crop[0] == '-') {
             tableData += `<tr><th colspan="8" class="text-center">${crop.slice(1)}</td></tr>`;
         } else {
-            let product, sell_price, sell_changes, buy_price, buy_changes, spread, buy_move, move_changes;
-            product = crop.split('_').map(capitalize).join(' ');
-            const marketCrop = market.products[crop.toUpperCase()];
+            const marketCrop = oldMarket[crop];
             if (!marketCrop) continue;
-            sell_price = averagePrice(marketCrop.sell_summary);
-            const s_old = oldMarket[product]?.sell_price;
-            sell_changes = s_old? (sell_price - s_old) / s_old * 100: 0;
-            buy_price = averagePrice(marketCrop.buy_summary);
-            const b_old = oldMarket[product]?.buy_price;
-            buy_changes = b_old? (buy_price - b_old) / b_old * 100: 0;
-            spread = buy_price? (buy_price - sell_price) / sell_price * 100 : 999;
-            if (spread > 999.99) spread = 999.99;
-            buy_move = marketCrop.quick_status.buyMovingWeek;
-            move_changes = buy_move - (oldMarket[product]?.buy_move ?? buy_move);
-            const {low, high} = config.alerts_dict[crop] ?? {};
-            const color_buy = buy_price <= low ? 'r' : (buy_price >= high ? 'g' : 'd');
-            const color_sell = sell_price >= high ? 'r' : (sell_price <= low ? 'g' : 'd');
-            const color_map = {r: ' class="table-danger"', g: ' class="table-success"', d: ''};
+            let product = crop.split('_').map(capitalize).join(' ');
             tableData += `<tr><th scope="row" class="text-start">${product}</th>
-                <td${color_map[color_sell]}>${formatNumber(sell_price)}</td>
-                <td>${formatNumber(sell_changes, 1) + ' %'}</td>
-                <td${color_map[color_buy]}>${formatNumber(buy_price)}</td>
-                <td>${formatNumber(buy_changes, 1) + ' %'}</td>
-                <td>${formatNumber(spread) + ' %'}</td>
-                <td>${formatNumber(buy_move, 0)}</td>
-                <td>${formatNumber(move_changes, 0)}</td></tr>\n`
-            oldMarket[product] = {sell_price, sell_changes, buy_price, buy_changes, spread, buy_move, move_changes};
+                <td${marketCrop.color_sell}>${formatNumber(marketCrop.sell_price)}</td>
+                <td>${formatNumber(marketCrop.sell_changes, 1) + ' %'}</td>
+                <td${marketCrop.color_buy}>${formatNumber(marketCrop.buy_price)}</td>
+                <td>${formatNumber(marketCrop.buy_changes, 1) + ' %'}</td>
+                <td>${formatNumber(marketCrop.spread) + ' %'}</td>
+                <td>${formatNumber(marketCrop.buy_move, 0)}</td>
+                <td>${formatNumber(marketCrop.move_changes, 0)}</td></tr>\n`
         }
     document.getElementById('tCrops').innerHTML = tableData;
+}
+
+
+function updateMarket(market) {
+    if (!market.success) return updateStatus('Error loading market data');
+    const crops = new Set();
+    for (let section in config.menu) 
+        for (let crop of config.menu[section]) crops.add(crop);
+    for (let alert of config.Alerts) crops.add(alert.name);
+
+    for (let crop of crops) updateCrop(crop, market.products[crop.toUpperCase()]);
+
     const lang = navigator.language || navigator.userLanguage;
     updateStatus('Last updated: ' + new Date(market.lastUpdated).toLocaleString(lang) + ` (${market.lastUpdated})`);
     updatedSuccessfully = true;
+    drawMarket();
 }
 
 function downloadMarket() {
@@ -111,17 +123,11 @@ function updateConfig(response) {
 
 function init() {
     selectedMenu = localStorage?.getItem?.(lsPrefix + 'selectedMenu');
-
     fetch('json/bazaar_monitored.json').then(res => res.json().then(res => updateConfig(res)))
-    // let request = new XMLHttpRequest();
-    // request.open("GET", 'json/bazaar_monitored.json');
-    // request.responseType = "json";
-    // request.send();
-    // request.onload = () => updateConfig(request.response);
 }
 
 function navClick(item) {
     selectedMenu = item.textContent;
     localStorage?.setItem?.(lsPrefix + 'selectedMenu', selectedMenu);
-    downloadMarket();
-}
+    drawMarket();
+} 
