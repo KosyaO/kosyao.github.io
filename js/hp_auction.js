f508c8b09a7ecac2dcd6f2cef73738066ada29d8
Александр Озеров <cdg.axel@gmail.com>
2024-11-02 04:44:56 +0300

    updated controls accessibility

--- js/hp_auction.js
+++ js/hp_auction.js
@@ -2,22 +2,6 @@ import { auctionDownload, auctionFilter, templates } from './auction.mjs';
 
 let searchProcessed = false;
 
-function armorChanged(element) {
-    const name = document.getElementById('armorName').value + ' ' + document.getElementById('partName').value;
-    document.getElementById('itemName').value = name;
-}
-
-function clearBtn() {
-    document.getElementById('attribute1').value = '';
-    document.getElementById('attribute2').value = '';
-}
-
-function templateSelect(event) {
-    const ctrl = document.getElementById('pnlParams');
-    if (event.target.value!=="") ctrl.classList.add('d-none');
-    else ctrl.classList.remove('d-none');
-}
-
 function fillTable(filtered, max_items = 999) {
     let found_total = 0, passed_total = 0;
     for (let item_name in filtered.counter) {
@@ -85,10 +69,49 @@ function searchBtn() {
     auctionSearch(filter);
 }
 
+function setDisabled(ctrlName, value) {
+    const ctrl = document.getElementById(ctrlName);
+    if (value) ctrl.setAttribute('disabled', 'true')
+        else ctrl.removeAttribute('disabled');
+}
+
+function setVisible(ctrlName, value) {
+    const ctrl = document.getElementById(ctrlName);
+    if (value) ctrl.classList.remove('d-none')
+        else ctrl.classList.add('d-none');
+}
+
+function clearBtn() {
+    document.getElementById('attribute1').value = '';
+    document.getElementById('attribute2').value = '';
+}
+
+function templateSelect(event) {
+    setVisible('pnlParams', event.target.value === "");
+}
+
+function armorChanged(element) {
+    const name = document.getElementById('armorName')
+    const part = document.getElementById('partName');
+    const nameValue = name.hasAttribute('disabled') ? '' : name.value;
+    const partValue = part.hasAttribute('disabled') ? '' : part.value;
+    document.getElementById('itemName').value = (nameValue + ' ' + partValue).trim();
+}
+
 function namePress(event) {
     if (event.key === "Enter") {
         searchBtn();
     }
+}
+
+function optionChanged() {
+    const attributeSearch = document.getElementById('attributeSearch').checked;
+    const tmpPart = document.getElementById('tOption2').checked;
+    const tmpName = document.getElementById('tOption3').checked;
+    setDisabled('attribute2', attributeSearch);
+    setDisabled('armorName', tmpPart);
+    setDisabled('partName', tmpName);
+    armorChanged(undefined);
 }
 
 function init() {
@@ -97,7 +120,8 @@ function init() {
         'click-clear': clearBtn,
         'change-armor': armorChanged,
         'keydown-name': namePress,
-        'change-template': templateSelect
+        'change-template': templateSelect,
+        'change-option': optionChanged
     }
     
     for (let [kind, handler] of Object.entries(handlers)) {
