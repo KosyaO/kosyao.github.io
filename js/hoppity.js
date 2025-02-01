92df6f3c19b9f4c57066405654b0e1619db5a7db
Александр Озеров <cdg.axel@gmail.com>
2025-02-01 11:58:29 +0300

    hoppity update

--- js/hoppity.js
+++ js/hoppity.js
@@ -1,41 +1,36 @@
 import { addHandlers, loadFromStorage, saveToStorage } from './hp_common.js';
 const prefix = "hoppity_";
-let endTime;
+let endTime, hitmanCd;
 
-function updateEndTime() {
-    const timeStr = endTime?.toLocaleString('en-GB', {dateStyle: "short", timeStyle: "short"});
-    document.getElementById('edEndTime').value = timeStr;
-}
+const timezone_offset = new Date().getTimezoneOffset() * 60;
+
+const secondsToStr = time => new Date(time + timezone_offset).toLocaleString('en-GB', {timeStyle: "medium"});
 
 function updateTimeEnd() {
     if (endTime === undefined) return;
-    let timeToEnd = endTime - new Date();
-    const hrs = Math.trunc(timeToEnd / 3600000);
-    const mins = Math.trunc((timeToEnd - hrs * 3600000) / 60000);
-    const secs = Math.trunc((timeToEnd - hrs * 3600000 - mins * 60000) / 1000);
-    const txt = `${hrs}:${mins}:${secs}`;
-    document.getElementById("lbTimeToEnd").textContent = txt;
+    let timeToEnd = endTime * 1000 - Date.now();
+    if (timeToEnd < 0) timeToEnd = 0;
+    document.getElementById("lbEndTime").textContent = secondsToStr(timeToEnd);
 }
 
-function calculateTime() {
-    let toendHrs = Number(document.getElementById("timeToEnd").value);
-    const slots = document.getElementById("edSlots").value;
-    const eggsCnt = document.getElementById("selEggsCnt").value;
-    const filledEggs = document.getElementById("edFilledEggs").value;
-    endTime = new Date();
-    if (endTime.getMinutes() > 55) toendHrs++;
-    endTime.setHours(endTime.getHours() + toendHrs, 55);
+function setEventEnd() {
+    let hours_left = Number(document.getElementById("timeToEnd").value);
+    const current_time = Math.trunc(Date.now() / 1000);
+    endTime = Math.trunc(current_time / 3600 + hours_left + (current_time % 3600 >= 3300)) * 3600 + 3300
     saveToStorage(prefix + 'endTime', endTime);
-    updateEndTime();
+}
+
+function setHitmanCooldown() {
+
 }
 
 function init() {
     addHandlers({
-        'click-calc': calculateTime,
+        'click-setend': setEventEnd,
+        'click-sethitman': setHitmanCooldown
     });
     const timeStr = loadFromStorage(prefix + 'endTime');
-    if (timeStr !== null) endTime = new Date(timeStr);
-    updateEndTime();
+    if (timeStr !== null) endTime = Number(timeStr);
     setInterval(updateTimeEnd, 1000);
 }
 
